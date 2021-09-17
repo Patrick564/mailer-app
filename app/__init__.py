fb77f11f6ec9c36ec9042ea4c63701ab20f7d6a8
Patrick Vilchez <pvilchez794@gmail.com>
2021-09-17 15:14:39 -0500

    First commit

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SENDGRID_KEY=os.environ.get('SENDGRID_API_KEY'),
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        DATABASE_HOST=os.environ.get('DATABASE_HOST'),
+        DATABASE_PASSWORD=os.environ.get('DATABASE_PASSWORD'),
+        DATABASE_USER=os.environ.get('DATABASE_USER'),
+        DATABASE=os.environ.get('DATABASE'),
+    )
+
+    from . import db
+
+    db.init_app(app)
+
+    return app
