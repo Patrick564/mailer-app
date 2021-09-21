561dc8627da5b072eae516904c2c6301d270bb5c
Patrick Vilchez <pvilchez794@gmail.com>
2021-09-21 15:22:18 -0500

    Sendgrid implemented and it routes.

--- app/mail.py
+++ app/mail.py
@@ -1,6 +1,10 @@
 from flask import (
-    Blueprint, render_template,
+    Blueprint, render_template, request,
+    flash, redirect, url_for, current_app
 )
+
+import sendgrid
+from sendgrid.helpers.mail import *
 
 from app.db import get_db
 
@@ -9,10 +13,62 @@ bp = Blueprint('mail', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET'])
 def index():
+    search = request.args.get('search')
     db, c = get_db()
 
-    c.execute('SELECT * FROM email')
+    if search is None:
+        c.execute('SELECT * FROM email')
+    else:
+        c.execute(
+            'SELECT * FROM email WHERE content LIKE %s',
+            ('%' + search + '%',)
+        )
+
     mails = c.fetchall()
 
     print(mails)
     return render_template('mails/index.html', mails=mails)
+
+
+@bp.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+        errors = []
+
+        if not email:
+            errors.append('Email is required.')
+
+        if not subject:
+            errors.append('Subject is required.')
+
+        if not content:
+            errors.append('Content is required.')
+
+        if len(errors) == 0:
+            send(email, subject, content)
+            db, c = get_db()
+            c.execute(
+                'INSERT INTO email (email, subject, content) VALUES (%s, %s, %s)',
+                (email, subject, content)
+            )
+            db.commit()
+
+            return redirect(url_for('mail.index'))
+        else:
+            for error in errors:
+                flash(error)
+
+    return render_template('mails/create.html')
+
+
+def send(to, subject, content):
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
+    from_email = Email(current_app.config['FROM_EMAIL'])
+    to_email = To(to)
+    content = Content('text/plain', content)
+    mail = Mail(from_email, to_email, subject, content)
+
+    response = sg.client.mail.send.post(request_body=mail.get())
